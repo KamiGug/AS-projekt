74f67602fcdedc87895fbd8374de5b294e75cad5
Kamil Gugala <gug.kamil@gmail.com>
2024-10-24 20:38:55 +0200

    init

---
+++ config/db.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\ArrayHelper;
+
+$db = [
+    'class' => 'yii\db\Connection',
+    'dsn' =>'',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    ...(defined('YII_ENV') && YII_ENV === 'PROD'
+        ? [
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 60,
+            'schemaCache' => 'cache',
+        ]
+        : []
+    )
+];
+
+if (defined('YII_ENV')) {
+    switch (YII_ENV) {
+        case 'PROD': 
+            $db = ArrayHelper::merge($db, require(__DIR__ . '/db/prod.php'));
+            break;
+        case 'DEV':
+            $db = ArrayHelper::merge($db, require(__DIR__ . '/db/dev.php'));
+            break;
+        case 'STAGE':
+            $db = ArrayHelper::merge($db, require(__DIR__ . '/db/stage.php'));
+            break;
+    }
+}
+
+return $db;
---
+++ config/db/dev.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dsn' =>'mysql:host=localhost:33068;dbname=kurnik-clone-table',
+    'username' => 'root',
+    'password' => 'Qk2P30B8dRn73JDAwf6EW3KQw7pgWs4G',
+    'charset' => 'utf8',
+];
